d03f92fdfe583b3d10cd8c625944d373b91da851
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-04-23 10:53:44 -0700

    Reworked logic gates
    * NAND/NOR/XNOR will now NOT the base gate (AND, NOR, and XOR
    respectively) instead of calculating everything in the opposite way as
    the base gate
    * All gates remade with ternary operators to be sexy

--- src/thepoptartcrpr/computer/components/gates/LogicGates.java
+++ src/thepoptartcrpr/computer/components/gates/LogicGates.java
@@ -11,38 +11,41 @@ public class LogicGates {
 	// so it would simply return this.not(this.xor(a, b)) instead of calculating on its own
 	
 	public int and(int a, int b) {
-		if (a == 1 && b == 1) return 1;
-		else return 0;
+		// if (a == 1 && b == 1) return 1;
+		// else return 0;
+		return a == 1 && b == 1 ? 1 : 0;
 	}
 	
 	public int nand(int a, int b) {
-		if (a == 1 && b == 1) return 0;
-		else return 1;
+		return this.not(this.and(a, b));
 	}
 	
 	public int xor(int a, int b) {
-		if (a == b) return 0;
-		else return 1;
+		// if (a == b) return 0;
+		// else return 1;
+		return a == b ? 0 : 1;
 	}
 	
 	public int xnor(int a, int b) {
-		if (a == b) return 1;
-		else return 0;
+		return this.not(this.xor(a, b));
 	}
 	
 	public int or(int a, int b) {
-		if (a == 1 || b == 1) return 1;
-		else return 0;
+		// if (a == 1 || b == 1) return 1;
+		// else return 0;
+		return a == 1 || b == 1 ? 1 : 0;
 	}
 	
 	public int not(int a) {
-		if (a == 1) return 0;
-		else return 1;
+		// if (a == 1) return 0;
+		// else return 1;
+		return a == 1 ? 0 : 1;
 	}
 	
 	public int nor(int a, int b) {
-		if (a == 0 && b == 0) return 1;
-		else return 0;
+		// if (a == 0 && b == 0) return 1;
+		// else return 0;
+		return a == 0 && b == 0 ? 1 : 0;
 	}
 
 }
